f05a2ff8b52cb0d2df10cf07e3e294023edd6242
Brian Wong <brianw7218@gmail.com>
2023-09-23 18:13:08 -0700

    Add CSV files and distance parser

---
+++ distance_import.py
@@ -0,0 +1,18 @@
+import csv
+
+def importDistance(distance_file):
+    distance_map = {}
+    with open(distance_file, encoding='utf-8-sig') as distances:
+        reader = csv.reader(distances, delimiter=",")
+        headers = next(reader)
+        for row in reader:
+            row_data = {}
+            for i, val in enumerate(row):
+                row_data[headers[i]] = float(val)
+            for header in headers:
+                if header not in distance_map:
+                    distance_map[header] = row_data
+                    break
+    return distance_map
+
+importDistance("distance.csv")
