05ac2cf8d407747eda6205a287a6d4476a055fd4
Brian Wong <brianw7218@gmail.com>
2023-09-24 15:59:59 -0700

    Add utility functions to be used for calculating and formatting truck delivery time

---
+++ timeUtils.py
@@ -0,0 +1,35 @@
+def timeStringToMins(string):
+    split1 = string.split(" ")
+    split2 = split1[0].split(":")
+
+    am_or_pm = split1[1]
+
+    hours = (int(split2[0]))
+    if hours == 12:
+        hours = 0
+    else:
+        hours *= 60
+    mins = int(split2[1])
+    if am_or_pm == "PM":
+        hours += 12 * 60
+
+    total_mins = hours + mins
+    return total_mins
+
+def minsToTimeString(time):
+    mins = int(time % 60)
+    hours = int((time - mins) / 60)
+    am_or_pm = "AM"
+    if hours == 0:
+        hours = 12
+    elif hours > 12:
+        am_or_pm = "PM"
+        hours -= 12
+    elif hours == 12:
+        am_or_pm = "PM"
+
+    # format minutes with leading zeros
+    if mins < 10:
+        mins = "0" + str(mins)
+
+    return f"{hours}:{mins} {am_or_pm}"
